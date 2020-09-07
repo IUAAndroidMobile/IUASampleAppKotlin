0c68d289503576498da4c0b9e40172abf83cba26
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2020-09-07 19:13:04 -0300

    add recycler view

---
+++ app/src/main/java/com/nicolasfanin/iuasampleappkotlin/ListAdapter.kt
@@ -0,0 +1,27 @@
+package com.nicolasfanin.iuasampleappkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter(val data: List<Color>) : RecyclerView.Adapter<ListAdapter.PalleteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PalleteViewHolder =
+        PalleteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_color_view, parent, false))
+
+    override fun onBindViewHolder(holder: PalleteViewHolder, position: Int) {
+        val color = data[position]
+        holder.titleTextView.text = color.name
+        holder.subtitleTextView.text = color.hex
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    class PalleteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val circleView = itemView.findViewById<View>(R.id.circleView)
+        val titleTextView = itemView.findViewById<TextView>(R.id.titleTextView)
+        val subtitleTextView = itemView.findViewById<TextView>(R.id.subtitleTextView)
+    }
+}
--- app/src/main/java/com/nicolasfanin/iuasampleappkotlin/MyMainActivity.kt
+++ app/src/main/java/com/nicolasfanin/iuasampleappkotlin/MyMainActivity.kt
@@ -3,11 +3,40 @@ package com.nicolasfanin.iuasampleappkotlin
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class MyMainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = ListAdapter(initColors())
+    }
+
+    private fun initColors(): List<Color> {
+        val colors = mutableListOf<Color>()
+
+        colors.add(Color(getString(R.string.blue), ContextCompat.getColor(this, R.color.blue).toString()))
+        colors.add(Color(getString(R.string.indigo), ContextCompat.getColor(this, R.color.indigo).toString()))
+        colors.add(Color(getString(R.string.red), ContextCompat.getColor(this, R.color.red).toString()))
+        colors.add(Color(getString(R.string.green), ContextCompat.getColor(this, R.color.green).toString()))
+        colors.add(Color(getString(R.string.orange), ContextCompat.getColor(this, R.color.orange).toString()))
+        colors.add(Color(getString(R.string.grey), ContextCompat.getColor(this, R.color.grey).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+
+        return colors
     }
 }
