ff4baa034a84d96ae2f5b954d4dda91c27c8163f
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2020-09-14 18:06:46 -0300

    Respect circle view in color item

--- app/src/main/java/com/nicolasfanin/iuasampleappkotlin/ListAdapter.kt
+++ app/src/main/java/com/nicolasfanin/iuasampleappkotlin/ListAdapter.kt
@@ -1,27 +1,42 @@
 package com.nicolasfanin.iuasampleappkotlin
 
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListAdapter(val data: List<Color>, val listener: RecyclerViewOnClickListener) : RecyclerView.Adapter<ListAdapter.PalleteViewHolder>() {
+class ListAdapter(val data: List<Color>, val listener: RecyclerViewOnClickListener) :
+    RecyclerView.Adapter<ListAdapter.PalleteViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PalleteViewHolder =
-        PalleteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_color_view, parent, false), listener)
+        PalleteViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_color_view,
+                parent,
+                false
+            ), listener
+        )
 
     override fun onBindViewHolder(holder: PalleteViewHolder, position: Int) {
         val color = data[position]
         holder.titleTextView.text = color.name
         holder.subtitleTextView.text = color.hex
 
-        holder.circleView.setBackgroundColor(android.graphics.Color.parseColor(color.hex))
+
+        //holder.circleView.setBackgroundColor(android.graphics.Color.parseColor(color.hex))
+
+        val gradientDrawable: GradientDrawable = holder.circleView.background as GradientDrawable
+        gradientDrawable.setColor(android.graphics.Color.parseColor(color.hex))
     }
 
     override fun getItemCount(): Int = data.size
 
-    class PalleteViewHolder(itemView: View, listener: RecyclerViewOnClickListener) : RecyclerView.ViewHolder(itemView) {
+    class PalleteViewHolder(itemView: View, listener: RecyclerViewOnClickListener) :
+        RecyclerView.ViewHolder(
+            itemView
+        ) {
         val circleView = itemView.findViewById<View>(R.id.circleView)
         val titleTextView = itemView.findViewById<TextView>(R.id.titleTextView)
         val subtitleTextView = itemView.findViewById<TextView>(R.id.subtitleTextView)
