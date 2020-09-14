142ae257b5242d99d28e5ae4e9cd6ecde5ce6539
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2020-09-14 17:44:25 -0300

    Add Clicklistener on recyclerview and set color

--- app/src/main/java/com/nicolasfanin/iuasampleappkotlin/ListAdapter.kt
+++ app/src/main/java/com/nicolasfanin/iuasampleappkotlin/ListAdapter.kt
@@ -6,22 +6,35 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListAdapter(val data: List<Color>) : RecyclerView.Adapter<ListAdapter.PalleteViewHolder>() {
+class ListAdapter(val data: List<Color>, val listener: RecyclerViewOnClickListener) : RecyclerView.Adapter<ListAdapter.PalleteViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PalleteViewHolder =
-        PalleteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_color_view, parent, false))
+        PalleteViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_color_view, parent, false), listener)
 
     override fun onBindViewHolder(holder: PalleteViewHolder, position: Int) {
         val color = data[position]
         holder.titleTextView.text = color.name
         holder.subtitleTextView.text = color.hex
+
+        holder.circleView.setBackgroundColor(android.graphics.Color.parseColor(color.hex))
     }
 
     override fun getItemCount(): Int = data.size
 
-    class PalleteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class PalleteViewHolder(itemView: View, listener: RecyclerViewOnClickListener) : RecyclerView.ViewHolder(itemView) {
         val circleView = itemView.findViewById<View>(R.id.circleView)
         val titleTextView = itemView.findViewById<TextView>(R.id.titleTextView)
         val subtitleTextView = itemView.findViewById<TextView>(R.id.subtitleTextView)
+
+        init {
+            itemView.setOnClickListener(View.OnClickListener {
+                listener.onItemClickListener(adapterPosition)
+            })
+        }
     }
+
+    interface RecyclerViewOnClickListener {
+        fun onItemClickListener(position: Int)
+    }
+
 }
--- app/src/main/java/com/nicolasfanin/iuasampleappkotlin/MyMainActivity.kt
+++ app/src/main/java/com/nicolasfanin/iuasampleappkotlin/MyMainActivity.kt
@@ -1,14 +1,15 @@
 package com.nicolasfanin.iuasampleappkotlin
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
-class MyMainActivity : AppCompatActivity() {
+class MyMainActivity : AppCompatActivity(), ListAdapter.RecyclerViewOnClickListener {
+
+    val colors = mutableListOf<Color>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,27 +17,31 @@ class MyMainActivity : AppCompatActivity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ListAdapter(initColors())
+        recyclerView.adapter = ListAdapter(initColors(), this)
     }
 
     private fun initColors(): List<Color> {
-        val colors = mutableListOf<Color>()
-
-        colors.add(Color(getString(R.string.blue), ContextCompat.getColor(this, R.color.blue).toString()))
-        colors.add(Color(getString(R.string.indigo), ContextCompat.getColor(this, R.color.indigo).toString()))
-        colors.add(Color(getString(R.string.red), ContextCompat.getColor(this, R.color.red).toString()))
-        colors.add(Color(getString(R.string.green), ContextCompat.getColor(this, R.color.green).toString()))
-        colors.add(Color(getString(R.string.orange), ContextCompat.getColor(this, R.color.orange).toString()))
-        colors.add(Color(getString(R.string.grey), ContextCompat.getColor(this, R.color.grey).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
-        colors.add(Color(getString(R.string.teal), ContextCompat.getColor(this, R.color.teal).toString()))
+        colors.add(Color(getString(R.string.blue), getString(R.string.blueHex)))
+        colors.add(Color(getString(R.string.indigo), getString(R.string.indigoHex)))
+        colors.add(Color(getString(R.string.red), getString(R.string.redHex)))
+        colors.add(Color(getString(R.string.green), getString(R.string.greenHex)))
+        colors.add(Color(getString(R.string.orange), getString(R.string.orangeHex)))
+        colors.add(Color(getString(R.string.grey), getString(R.string.greyHex)))
+        colors.add(Color(getString(R.string.teal), getString(R.string.tealHex)))
+        colors.add(Color(getString(R.string.brown), getString(R.string.brownHex)))
+        colors.add(Color(getString(R.string.blue), getString(R.string.blueHex)))
+        colors.add(Color(getString(R.string.indigo), getString(R.string.indigoHex)))
+        colors.add(Color(getString(R.string.red), getString(R.string.redHex)))
+        colors.add(Color(getString(R.string.green), getString(R.string.greenHex)))
+        colors.add(Color(getString(R.string.orange), getString(R.string.orangeHex)))
+        colors.add(Color(getString(R.string.grey), getString(R.string.greyHex)))
+        colors.add(Color(getString(R.string.teal), getString(R.string.tealHex)))
+        colors.add(Color(getString(R.string.brown), getString(R.string.brownHex)))
 
         return colors
+    }
+
+    override fun onItemClickListener(position: Int) {
+        Toast.makeText(this, "Su Color es ${colors[position].name}", Toast.LENGTH_SHORT).show()
     }
 }
