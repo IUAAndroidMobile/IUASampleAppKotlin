70f85dbfc46a8b2a7661f9d9356d71480a718b82
nicolasfanin <nicolasfanin.id@pinapp.pe>
2021-09-27 08:50:14 -0300

    Agregar listener a recycler view

--- app/src/main/java/com/nicolasfanin/iuasampleappkotlin/recyclerView/Color.kt
+++ app/src/main/java/com/nicolasfanin/iuasampleappkotlin/recyclerView/Color.kt
@@ -1,6 +1,34 @@
 package com.nicolasfanin.iuasampleappkotlin.recyclerView
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Color(
-    val name: String,
-    val hex: String
-)
+    val name: String? = null,
+    val hex: String? = null
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(hex)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Color> {
+        override fun createFromParcel(parcel: Parcel): Color {
+            return Color(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Color?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
