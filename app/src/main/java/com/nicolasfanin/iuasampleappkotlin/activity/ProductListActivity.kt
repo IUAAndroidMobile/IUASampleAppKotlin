ceeac301ae6b39149e3805960131e0e6880edb44
nicolasfanin <faninnicolas@gmail.com>
2022-10-21 15:29:33 -0300

    Se agrega Glide, navigation drawer sin terminar y paso de objetos entre activities.

--- app/src/main/java/com/nicolasfanin/iuasampleappkotlin/activity/ProductListActivity.kt
+++ app/src/main/java/com/nicolasfanin/iuasampleappkotlin/activity/ProductListActivity.kt
@@ -1,5 +1,6 @@
 package com.nicolasfanin.iuasampleappkotlin.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -24,9 +25,9 @@ class ProductListActivity : AppCompatActivity(), ProductListOnClickListener {
     }
 
     private fun createProducts() = mutableListOf<Product>(
-        Product(title = "Producto 1", description = "Descripcion del producto 1", imageURL = "http://...", 500.0),
-        Product(title = "Producto 2", description = "Descripcion del producto 2", imageURL = "http://...", 500.0),
-        Product(title = "Producto 3", description = "Descripcion del producto 3", imageURL = "http://...", 500.0),
+        Product(title = "Producto 1", description = "Descripcion del producto 1", imageURL = "https://www.pruebaderuta.com/wp-content/uploads/2015/05/superautos.jpg", 500.0),
+        Product(title = "Producto 2", description = "Descripcion del producto 2", imageURL = "https://fotos.perfil.com//2019/10/07/900/0/cuales-son-los-autos-mas-potentes-del-mundo-788465.jpg", 500.0),
+        Product(title = "Producto 3", description = "Descripcion del producto 3", imageURL = "https://www.pruebaderuta.com/wp-content/uploads/2015/05/superautos.jpg", 500.0),
         Product(title = "Producto 4", description = "Descripcion del producto 4", imageURL = "http://...", 500.0),
         Product(title = "Producto 5", description = "Descripcion del producto 5", imageURL = "http://...", 500.0),
         Product(title = "Producto 6", description = "Descripcion del producto 6", imageURL = "http://...", 500.0),
@@ -44,8 +45,14 @@ class ProductListActivity : AppCompatActivity(), ProductListOnClickListener {
         Product(title = "Producto 6", description = "Descripcion del producto 6", imageURL = "http://...", 500.0))
 
     override fun onItemClick(position: Int) {
+
         Toast.makeText(baseContext, "Su Producto seleccionado es: ${productList[position].title}", Toast.LENGTH_SHORT).show()
 
+        val myIntent = Intent(this, MyIntentActivity::class.java)
+        myIntent.putExtra("product",productList[position])
+        startActivity(myIntent)
 
     }
+
+
 }
